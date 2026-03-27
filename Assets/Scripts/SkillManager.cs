908cc0fc7c3fac5636918bb28ebca95866243806
Diego Benites <banitessebastian2decundaria@gmail.com>
2026-03-26 20:08:45 -0500

    =)

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -5,26 +5,4 @@ using UnityEngine;
 public class SkillManager : ScriptableObject
 {
     public Skill[] allSkills;
-
-    public static void Process<T>(T element, Action<T> action)
-    {
-        action(element);
-    }
-
-    public static TResult Transform<T, TResult>(T element, Func<T, TResult> func)
-    {
-        return func(element);
-    }
-
-    public static bool Validate<T>(T element, Func<T, bool> predicate)
-    {
-        if (element == null || predicate == null)
-            return false;
-        else
-        {
-            return predicate(element);
-        }
-
-
-    }
 }
